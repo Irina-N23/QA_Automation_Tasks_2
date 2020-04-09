c5d1b868bcad389c02a897d74610b5eb1609157a
Irina Novik <solvejg.23@gmail.com>
2020-04-09 16:26:16 +0300

    Add three classes of my I/O Fundamentals task

---
+++ Java/src/main/java/automation/iofundamentals/DirectoryTreeFileAnalyser.java
@@ -0,0 +1,92 @@
+package automation.iofundamentals;
+
+import java.io.*;
+
+class DirectoryTreeFileAnalyser {
+    private int totalNumberOfDirectories;
+    private int totalNumberOfFiles;
+
+    void readTextFile() {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(DirectoryTreeWriter
+                                                                               .directoryTree))) {
+            bufferedReader.lines().forEach(System.out::println);
+        } catch (IOException exception) {
+            System.err.print("File reading has been failed:\n" + exception);
+        }
+    }
+
+    int getNumberOfDirectories() {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(DirectoryTreeWriter
+                                                                               .directoryTree))) {
+            String line;
+            totalNumberOfDirectories = 0;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.contains(DirectoryTreeWriter.directoryTab)) {
+                    totalNumberOfDirectories++;
+                }
+            }
+        } catch (IOException exception) {
+            System.err.print("File reading has been failed:\n" + exception);
+        }
+        return totalNumberOfDirectories;
+    }
+
+    int getNumberOfFiles() {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(DirectoryTreeWriter
+                                                                               .directoryTree))) {
+            String line;
+            totalNumberOfFiles = 0;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.contains(DirectoryTreeWriter.fileTab)) {
+                    totalNumberOfFiles++;
+                }
+            }
+        } catch (IOException exception) {
+            System.err.print("File reading has been failed:\n" + exception);
+        }
+        return totalNumberOfFiles;
+    }
+
+    double getAverageNumberOfFilesInDirectory() {
+        totalNumberOfDirectories = 0;
+        totalNumberOfFiles = 0;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(DirectoryTreeWriter
+                                                                               .directoryTree))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.contains(DirectoryTreeWriter.directoryTab)) {
+                    totalNumberOfDirectories++;
+                }
+                if (line.contains(DirectoryTreeWriter.fileTab)) {
+                    totalNumberOfFiles++;
+                }
+            }
+        } catch (IOException exception) {
+            System.err.print("File reading has been failed:\n" + exception);
+        }
+        return (totalNumberOfDirectories == 0) ? totalNumberOfFiles :
+                                             totalNumberOfFiles / (double) totalNumberOfDirectories;
+    }
+
+    double getAverageFileNameLength() {
+        totalNumberOfFiles = 0;
+        double totalFileNameLength = 0.0;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(DirectoryTreeWriter
+                                                                               .directoryTree))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.matches(("^(\\|(\\s{4}))+(.+)(\\.\\w{2,4}\\d*)$"))) {
+                    String changedLine = (line.replaceAll("^(\\|(\\s{4}))+", "")
+                                              .replaceAll("(\\.\\w{2,4}\\d*)$", ""));
+                    totalNumberOfFiles++;
+                    totalFileNameLength += changedLine.length();
+                }
+            }
+        } catch (IOException exception) {
+            System.err.print("File reading has been failed:\n" + exception);
+        }
+        return totalFileNameLength / totalNumberOfFiles;
+    }
+}
---
+++ Java/src/main/java/automation/iofundamentals/DirectoryTreeWriter.java
@@ -0,0 +1,44 @@
+package automation.iofundamentals;
+
+import java.io.*;
+
+class DirectoryTreeWriter {
+    static String fileTab = "|    ";
+    static String directoryTab = "|----";
+    private StringBuilder treeContent = new StringBuilder();
+    static File directoryTree = new File("src" + File.separator + "main" + File.separator + "java"
+                                         + File.separator + "automation" + File.separator + "io"
+                                         + "fundamentals" + File.separator + "DirectoryTree.txt");
+
+    private StringBuilder getDirectoryTree(File directory, int depthLevel) {
+        if (depthLevel == 0) {
+            treeContent.append(directory.getName()).append("\n");
+        }
+
+        for (File item : directory.listFiles()) {
+            if (item.isFile()) {
+                for (int i = 0; i <= depthLevel; i++) {
+                    treeContent.append(fileTab);
+                }
+                treeContent.append(item.getName()).append("\n");
+            } else if (item.isDirectory()) {
+                for (int i = 0; i <= depthLevel; i++) {
+                    treeContent.append(directoryTab);
+                }
+                treeContent.append("[").append(item.getName()).append("]\n");
+                getDirectoryTree(item, depthLevel + 1);
+            }
+        }
+        return treeContent;
+    }
+
+    void writeTextFileWithDirectoryTree(File chosenDirectory) {
+        try (PrintWriter printWriter = new PrintWriter(new BufferedWriter(
+                                                           new FileWriter(directoryTree)))) {
+            printWriter.print(getDirectoryTree(chosenDirectory, 0).toString());
+            System.out.println("File \"DirectoryTree.txt\" has been successfully written.");
+        } catch (IOException exception) {
+            System.err.println("File writing has been failed:\n" + exception);
+        }
+    }
+}
